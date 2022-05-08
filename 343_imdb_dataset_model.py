3091eae5c679f185b64231b3d98c05019bcedf83
dokpin <dokpin@outlook.kr>
2022-05-08 19:53:23 +0900

    22.05.08 이진 분류

--- 343_imdb_dataset_model.py
+++ 343_imdb_dataset_model.py
@@ -20,19 +20,29 @@ def vectorize_sequence(sequences, dimension=10000):
 #print([max(sequence) for sequence in train_data])
 
 # 데이터 디코딩
-word_index = imdb.get_word_index()
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
-print(decoded_review)
+#word_index = imdb.get_word_index()
+#reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+#decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+#print(decoded_review)
 
 # 정수 시퀀스 인코딩
 x_train = vectorize_sequence(train_data)
 x_test = vectorize_sequence(test_data)
-print("train_data[0] vector")
-print(x_train[0])
+#print("train_data[0] vector")
+#print(x_train[0])
 
 # 레이블 인코딩
 y_train = np.asarray(train_labels).astype("float32")
 y_test = np.asarray(test_labels).astype("float32")
-print("y_train[0] vector")
-print(y_train[0])
+#print("y_train[0] vector")
+#print(y_train[0])
+
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
