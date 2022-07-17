2c484b6169718fe116ddc781b50eb6b9a8bb8e62
dokpin <dokpin@outlook.kr>
2022-07-18 07:00:39 +0900

    22.07.18

---
+++ 631_jena_dataset.py
@@ -0,0 +1,15 @@
+import os
+
+data_dir = './'
+fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+print(header)
+print(len(lines))
---
+++ 632_jena_dataset_prepare.py
@@ -0,0 +1,75 @@
+import os
+
+data_dir = './'
+fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+#print(header)
+#print(lines[0])
+#print(len(lines))
+
+# parsing data
+import numpy as np
+
+float_data = np.zeros((len(lines), len(header) - 1))
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',') [1:]]
+    #values = [int(x) for x in line.split(',') [1:]]
+    float_data[i, :] = values
+
+from matplotlib import pyplot as plt
+
+#temp = float_data[:, 1]
+#plt.plot(range(len(temp)), temp)
+#plt.plot(range(1440), temp[:1440])
+#plt.show()
+
+# 데이터 정규화(20만개 데이터를 학습으로 사용)
+mean = float_data[:200000].mean(axis=0)
+float_data -= mean
+std = float_data[:200000].std(axis=0)
+float_data /= std
+
+# 시계열 데이터와 타깃을 반환하는 제너레이터 함수
+def generator(data, lookback, delay, min_index, max_index, shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lockback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lockback, max_index, size=batch_size
+            )
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arrange(i, min(i + batch_size, max_index))
+            i += len(rows)
+    
+        samples = np.zeros((len(rows), lookback // step, data.shape[-1]))
+        targets = np.zeros((len(rows), ))
+
+        for j. row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+    
+
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+train_gen = generator(float_data, lookback=lookback, delay=delay, min_index=0, max_index=200000, shuffle=True, step=step, batch_size=batch_size)
+val_gen = generator(float_data, lookback=lookback, delay=delay, min_index=200001, max_index=300000, step=step, batch_size=batch_size)
+test_gen = generator(float_data, lookback=lookback, delay=delay, min_index=300001, max_index=None, step=step, batch_size=batch_size)
+
+val_steps = (300000 - 200001 - lookback) // batch_size
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
